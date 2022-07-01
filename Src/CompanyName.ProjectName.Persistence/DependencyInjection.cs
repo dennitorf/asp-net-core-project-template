ccffb7ba4dee6339774b49e98e785fd3c41a1abd
Denny Rodriguez Fajardo <dennitorf@gmail.com>
2022-07-01 11:11:21 -0400

    adding persistence DI

---
+++ Src/CompanyName.ProjectName.Persistence/DependencyInjection.cs
@@ -0,0 +1,34 @@
+﻿using CompanyName.ProjectName.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace CompanyName.ProjectName.Persistence
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+        {
+            string connectionString = Environment.GetEnvironmentVariable("DatabaseConnectionString");
+            connectionString = String.IsNullOrEmpty(connectionString) ? configuration.GetConnectionString("DatabaseConnectionString") : connectionString;
+
+            if (configuration.GetValue<bool>("UseInMemoryDatabase"))
+            {
+                services.AddDbContext<AppDbContext>(options =>
+                {
+                    options.UseInMemoryDatabase("CompanyNameProjectName"); ;
+                });
+            }
+            else
+            {
+                services.AddDbContext<AppDbContext>(options =>
+                {
+                    options.UseSqlServer(connectionString, b => b.MigrationsAssembly(typeof(AppDbContext).Assembly.FullName));
+                });
+            }
+
+            return services;
+        }
+    }
+}
