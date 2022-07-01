a7ff1d9ae8adf66a16deef8a547b9f40d5bc1b86
Denny Rodriguez Fajardo <dennitorf@gmail.com>
2022-07-01 11:43:25 -0400

    first controller ready

---
+++ Src/CompanyName.ProjectName.Application/Features/Todos/Queries/GetAllTodos/TodoDto.cs
@@ -0,0 +1,16 @@
+﻿using CompanyName.ProjectName.Application.Common.Interfaces.Mappings;
+using CompanyName.ProjectName.Domain.Entities.Sample;
+using System;
+
+namespace CompanyName.ProjectName.Application.Features.Todos.Queries.GetAllTodos
+{
+    public class TodoDto : IMapFrom<Todo>
+    {
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public string LastModifiedBy { get; set; }
+        public bool IsActive { set; get; }
+    }
+}
---
+++ Src/CompanyName.ProjectName.WebApi/Controllers/Todos.cs
@@ -0,0 +1,20 @@
+﻿using CompanyName.ProjectName.Application.Features.Todos.Queries.GetAllTodos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CompanyName.ProjectName.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Todos : BaseController
+    {
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<TodoDto>))]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await Mediator.Send(new GetAllTodosQuery() { }));
+        }
+    }
+}
