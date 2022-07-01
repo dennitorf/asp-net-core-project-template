2daba00a6c4afd2e845e9ed1898372f6f391cb36
Denny Rodriguez Fajardo <dennitorf@gmail.com>
2022-07-01 10:47:33 -0400

    creating static seeder, call it later

---
+++ Src/CompanyName.ProjectName.Persistence/Seeders/AppDbContextSeeder.cs
@@ -0,0 +1,20 @@
+﻿using CompanyName.ProjectName.Persistence.Contexts;
+
+namespace CompanyName.ProjectName.Persistence.Seeders
+{
+    public class AppDbContextSeeder
+    {
+        public static void Initialize(AppDbContext db)
+        {
+            var intitializer = new AppDbContextSeeder();
+        }
+
+        public void SeedEverything(AppDbContext db)
+        { 
+            db.Database.EnsureCreated();    
+        }
+
+        // Add your own seed methods 
+
+    }
+}
