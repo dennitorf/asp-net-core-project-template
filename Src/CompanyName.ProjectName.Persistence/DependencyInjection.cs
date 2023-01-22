dfc78886be5ed4dc0983dcc4920623ec853b1890
Denny Rodriguez Fajardo <dennitorf@gmail.com>
2023-01-21 23:30:36 -0500

    migrating DB, and adding environments files (#9)

--- Src/CompanyName.ProjectName.Persistence/DependencyInjection.cs
+++ Src/CompanyName.ProjectName.Persistence/DependencyInjection.cs
@@ -1,4 +1,5 @@
 ﻿using CompanyName.ProjectName.Persistence.Contexts;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -10,8 +11,6 @@ namespace CompanyName.ProjectName.Persistence
     {
         public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
         {
-            string connectionString = Environment.GetEnvironmentVariable("DatabaseConnectionString");
-            connectionString = String.IsNullOrEmpty(connectionString) ? configuration.GetConnectionString("DatabaseConnectionString") : connectionString;
 
             if (configuration.GetValue<bool>("UseInMemoryDatabase"))
             {
@@ -24,11 +23,41 @@ namespace CompanyName.ProjectName.Persistence
             {
                 services.AddDbContext<AppDbContext>(options =>
                 {
-                    options.UseSqlServer(connectionString, b => b.MigrationsAssembly(typeof(AppDbContext).Assembly.FullName));
+                    options.UseSqlServer(GetConnectionString(configuration) , b => b.MigrationsAssembly(typeof(AppDbContext).Assembly.FullName));
                 });
             }
 
             return services;
+        }
+
+        private static string GetConnectionString(IConfiguration configuration) 
+        {
+            string cs = "";
+
+            if (!String.IsNullOrEmpty(Environment.GetEnvironmentVariable("DB_SERVER")) &&
+                !String.IsNullOrEmpty(Environment.GetEnvironmentVariable("DB_USER")) &&
+                !String.IsNullOrEmpty(Environment.GetEnvironmentVariable("DB_PASSWORD")) &&
+                !String.IsNullOrEmpty(Environment.GetEnvironmentVariable("DB_DATABASE"))) 
+            {
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+
+                builder.DataSource = Environment.GetEnvironmentVariable("DB_SERVER");
+                builder.UserID = Environment.GetEnvironmentVariable("DB_USER");
+                builder.Password = Environment.GetEnvironmentVariable("DB_PASSWORD");
+                builder.InitialCatalog = Environment.GetEnvironmentVariable("DB_DATABASE");
+                builder.MultipleActiveResultSets = true;
+                builder.TrustServerCertificate = true;
+
+                return builder.ConnectionString;
+            }                
+            else 
+            {                                
+                cs = configuration.GetConnectionString("DatabaseConnectionString");
+            }
+
+            Console.WriteLine($"CS: {cs}");
+
+            return cs;
         }
     }
 }
--- Src/CompanyName.ProjectName.WebApi/Program.cs
+++ Src/CompanyName.ProjectName.WebApi/Program.cs
@@ -1,19 +1,45 @@
+using System;
+using System.Threading.Tasks;
+using CompanyName.ProjectName.Persistence.Contexts;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace CompanyName.ProjectName.WebApi
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public async static Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services  = scope.ServiceProvider; 
+
+                try 
+                {
+                    var context = services.GetRequiredService<AppDbContext>();
+
+                    if (context.Database.IsSqlServer())
+                    {
+                        await context.Database.MigrateAsync();
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+
+                    logger.LogError(ex, "An error occurred while migrating or seeding the database.");
+
+                    throw;
+                }
+            }
+            
+            await host.RunAsync();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
